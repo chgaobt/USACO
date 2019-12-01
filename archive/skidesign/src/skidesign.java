6dd02ec8730e19e520dc02f000becadc8be248a9
CGao2023 <58396550+CGao2023@users.noreply.github.com>
2019-12-01 15:20:28 -0500

    Update skidesign.java

--- archive/skidesign/src/skidesign.java
+++ archive/skidesign/src/skidesign.java
@@ -20,37 +20,13 @@ public class skidesign {
                 g = hillSize[i];
             }
         }
-        int count = 0;
         int start = 0;
-        stat.add(start);
-        for (int i = 0; i < numHills; i++) {
-            int num = 0;
-            for (int j = 0; j < numHills; j++) {
-                int x = (hillSize[i] + 17);
-                if (x < g) {
-                    int y = hillSize[j];
-                    if (y <= x && y >= (x - 17)) {
-                        num = num + 1;
-                    }
-                }
-            }
-            if (num > count) {
-                count = num;
-                start = i;
-            }
-
-            if(num == count) {
-                start = i;
-                stat.add(start);
-            }
-        }
         int cost = 900000000;
         for (int k = 0; k < g; k++) { // try all possible situations; the values in between the hill size are possible too
             start = k;
             int price = 0;
             for (int i = 0; i < numHills; i++) {
                 int z = hillSize[i];
-
                 if (z < start) {
                     int dif = start - z;
                     price = price + (dif * dif);
