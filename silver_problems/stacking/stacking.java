0ba7160c7e8f166de02258b4c82448814b1345f8
CGao2023 <chloejgao@gmail.com>
2022-11-25 21:16:12 -0500

    stacking done

--- silver_problems/stacking/stacking.java
+++ silver_problems/stacking/stacking.java
@@ -7,6 +7,24 @@ public class stacking {
         StringTokenizer st = new StringTokenizer(f.readLine()); 
         int n = Integer.parseInt(st.nextToken()); 
         int k = Integer.parseInt(st.nextToken()); 
-        System.out.println("done"); 
+        
+        int[] diff = new int[n+1];
+        int[] prefix = new int[n+1]; 
+
+        for(int i = 0; i<k; i++) {
+            st = new StringTokenizer(f.readLine()); 
+            int a = Integer.parseInt(st.nextToken()); 
+            int b = Integer.parseInt(st.nextToken()); 
+            diff[a-1] += 1; 
+            diff[b] -= 1; 
+        }
+
+        for(int i = 1; i<n+1; i++) {
+            prefix[i] = prefix[i-1] + diff[i];
+        }
+        Arrays.sort(prefix);
+        PrintWriter out = new PrintWriter(new FileWriter("stacking.out")); 
+        out.print(prefix[prefix.length/2]);
+        out.close();
     }
 }
