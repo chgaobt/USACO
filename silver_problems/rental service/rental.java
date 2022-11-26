73a0b2b70ee6165a238a663e10ce8bac01fa6143
CGao2023 <chloejgao@gmail.com>
2022-11-26 02:08:00 -0500

    11/26

---
+++ silver_problems/rental service/rental.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class rental {
+    public static void main (String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("rental.in")); 
+        
+    }
+}
