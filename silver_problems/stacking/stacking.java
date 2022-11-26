9e5259730b219a0e8e85865ad1cc3ed60abfbe6c
CGao2023 <chloejgao@gmail.com>
2022-11-25 21:04:51 -0500

    stacking problem

---
+++ silver_problems/stacking/stacking.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class stacking {
+    public static void main (String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("stacking.in")); 
+        StringTokenizer st = new StringTokenizer(f.readLine()); 
+        int n = Integer.parseInt(st.nextToken()); 
+        int k = Integer.parseInt(st.nextToken()); 
+        System.out.println("done"); 
+    }
+}
