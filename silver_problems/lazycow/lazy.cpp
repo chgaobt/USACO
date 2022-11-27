37ac32d9e7c7bab5ebf9ff5bb7a08fd8030feace
CGao2023 <chloejgao@gmail.com>
2022-11-26 19:58:16 -0500

    lazy changes

--- silver_problems/lazycow/lazy.cpp
+++ silver_problems/lazycow/lazy.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream> 
+
 using namespace std; 
 
 int main() {
-    cout << "Hello World"; 
+    ifstream fin ("lazy.in");
+    ofstream fout ("lazy.out"); 
+
+    int n; 
+    int k; 
+
+    fin >> n; 
+    fin >> k; 
+
+    cout << n;
+    cout <<k;
+    
     return 0; 
 }
+
